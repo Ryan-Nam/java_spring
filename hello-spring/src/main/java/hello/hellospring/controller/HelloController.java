aeb9e51ae169c0170d845a05b5be18461971589c
Ryan-Nam <alfofh7060@gmail.com>
2021-07-31 18:26:29 +1000

    Add controller, template get method

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,12 +3,19 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
-        return "hello";
+        return "hello"; // move to hello.html TEMPLATE
+    }
+
+    @GetMapping("hello-mvc")
+    public String hellowMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template"; // move to hello-template TEMPLATE
     }
 }
